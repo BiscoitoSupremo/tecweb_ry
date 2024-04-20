0ac1781c9170ad74dd10ebcb7909c6723da96bed
programer <132757736+BiscoitoSupremo@users.noreply.github.com>
2024-04-20 13:42:43 -0300

    Add files via upload

---
+++ conversao3.js
@@ -0,0 +1,6 @@
+A = parseFloat(prompt("Informe o valor da altura da lata"));
+R = parseFloat(prompt("Informe o valor do raio da lata"));
+V = 3.14159 * (R * R) * A;
+
+console.log("o valor da lata é"+ V.toFixed(2));
+alert("o valor da lata é"+ V.toFixed(2));
