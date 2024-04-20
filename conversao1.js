df7b5ab8f4afc8eea16650b3b40a35d08bf12e1e
programer <132757736+BiscoitoSupremo@users.noreply.github.com>
2024-04-20 13:53:17 -0300

    Delete conversao1.js

--- conversao1.js
+++
@@ -1,6 +0,0 @@
-C = parseFloat(prompt("Digite a temperatura em Celsius: "));
-
-F = (9 * C + 160) / 5;
-
-console.log("A temperatura convertida para Fahrenheit é \n"+ F)
-alert("A temperatura convertida para Fahrenheit é \n"+ F)
