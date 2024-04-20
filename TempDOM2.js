f675f39efd477aa183cea0cfcbadbccc2f1325a2
programer <132757736+BiscoitoSupremo@users.noreply.github.com>
2024-04-20 10:58:28 -0300

    Add files via upload

---
+++ TempDOM2.js
@@ -0,0 +1,10 @@
+function transforme(){
+
+    F = document.getElementById("inpF").value;
+    C = (F-32) * (5/9);
+
+console.log("A temperatura convertida para Celsius é \n" + C)
+alert("A temperatura convertida para Celsius é \n" + C)
+    alert("De nada (ง'̀-'́)ง")
+
+}
