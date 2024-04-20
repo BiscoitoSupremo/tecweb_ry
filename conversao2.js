249c4cbb65563f00a4ce3919ba471083a80fbeaf
programer <132757736+BiscoitoSupremo@users.noreply.github.com>
2024-04-20 13:52:57 -0300

    Delete conversao2.js

--- conversao2.js
+++
@@ -1,7 +0,0 @@
-// declaracao de variaveis
-F = parseFloat(prompt("Digite a temperatura em Fahrenheit: "));
-
-C = (F-32) * (5/9);
-// variavel de saida
-console.log("A temperatura convertida para Celsius é \n" + C)
-alert("A temperatura convertida para Celsius é \n" + C)
